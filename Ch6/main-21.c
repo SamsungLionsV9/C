73342dddc8abb9ad35ae970e2109ae4b74ff7475
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-29 23:16:19 +0900

    Add files via upload

---
+++ Ch6/main-21.c
@@ -0,0 +1,27 @@
+#pragma warning(disable: 4996)
+#include <stdio.h>
+
+int main()
+{   
+    int salary, expense, target;
+    int total = 0;
+    int month = 0;
+    
+    printf("월급:");
+    scanf("%d", &salary);
+    
+    printf("매달 지출:");
+    scanf("%d", &expense);
+    
+    printf("목표 금액:");
+    scanf("%d", &target);
+    
+    while(total < target){
+        total += (salary - expense);
+        month++;
+    }
+    printf("목표 금액까지 앞으로 %d개월 남았습니다.", month);
+        
+    
+    return 0;
+}
