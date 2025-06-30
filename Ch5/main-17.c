272b3c91b3abab28de1057f4342ee3167f42b6d8
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 13:30:15 +0900

    Update main-17.c

--- Ch5/main-17.c
+++ Ch5/main-17.c
@@ -6,7 +6,8 @@ int main()
     
     printf("연도를 입력하세요: ");
     scanf("%d", &year);
-    
+
+    //AND연산자 &&로 두 조건을 동시에 만족시킬때를 찾음
     if ( (year % 4 == 0 && year % 100 !=0 ) || year % 400 == 0){
         printf("윤년입니다.\n");
     }
