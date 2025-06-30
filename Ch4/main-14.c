398784057135790d6804d4f654f4ea3d1c8bcaa6
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 13:18:15 +0900

    Update main-14.c

--- Ch4/main-14.c
+++ Ch4/main-14.c
@@ -13,6 +13,9 @@ int main()
     
     printf("파랑(0 ~ 255): ");
     scanf("%d", &blue);
+
+    //red: 값을 16비트(2바이트) 왼쪽으로 이동
+    //green: 값을 8비트(1바이트) 왼쪽으로 이동
     
     int color = (red << 16) | (green << 8) | blue;
     printf("현재 색상: #%06X\n", color);
