ebc5c3999cedd2aa04a76947a8aa9c4e87f926ce
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 13:36:46 +0900

    Update main-21.c

--- Ch6/main-21.c
+++ Ch6/main-21.c
@@ -15,7 +15,8 @@ int main()
     
     printf("목표 금액:");
     scanf("%d", &target);
-    
+
+    //월급-고정지출을 달마다 total변수에 대입 후 달마다 합산
     while(total < target){
         total += (salary - expense);
         month++;
