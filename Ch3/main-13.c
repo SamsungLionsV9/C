66a70400cf9820f746415ac5b67c0e810e2a50e8
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 13:01:55 +0900

    Update main-13.c

--- Ch3/main-13.c
+++ Ch3/main-13.c
@@ -5,14 +5,15 @@
 int main()
 {
     double price;
-    double totalPrice = 0.0;
+    double totalPrice = 0.0; //totalPrice를 0으로 초기화 하는 이유?
+    //->price를 하단에서 새로 EXTRA 상수를 곱해서 연산하므로 값이 바뀌기 때문에 초기화를 해주어야함
     
     printf("가격을 입력하세요: ");
     scanf("%lf", &price);
     
     totalPrice = price + (price * EXTRA);
     
-    printf("최종가격: %.2lf원\n", totalPrice);
+    printf("최종가격: %.2lf원\n", totalPrice); //구한 totalPrice를 소수점 두자리까지 출력한다는 의미
 
     return 0;
 }
