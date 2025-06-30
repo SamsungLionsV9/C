00364a4b42ae8aeffaba2d17e624f776715b24b4
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 12:39:49 +0900

    Update main-2.c

--- Ch2/main-2.c
+++ Ch2/main-2.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    printf("정수: %d\n", 100);
-    printf("실수: %f\n", 10.123);
-    printf("문자: %c\n", 'A');
-    printf("문자열: %s\n", "Hello, C!!");
+    printf("정수: %d\n", 100); //정수 100 출력
+    printf("실수: %f\n", 10.123); //실수 10.123 출력
+    printf("문자: %c\n", 'A');    //문자 A 출력
+    printf("문자열: %s\n", "Hello, C!!"); //문자열 출력
     
     return 0;
 }
