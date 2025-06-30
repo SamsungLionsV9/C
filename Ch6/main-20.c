6f2ce8ba5ffd4000fc164dc55e0044fa89003434
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 13:35:56 +0900

    Update main-20.c

--- Ch6/main-20.c
+++ Ch6/main-20.c
@@ -4,7 +4,8 @@ int main()
 {
     int dan = 2;
     int num = 1;
-    
+
+    //구구단 2단만 출력, dan은 2로 고정되어있는걸 확인
     for(num = 1 ; num <=9; num++){
         printf("%d * %d = %d\n", dan, num, dan * num);
         
