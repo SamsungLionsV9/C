464ade4b0b7eb77ca27c12e29d33bc75153bd06c
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 13:34:34 +0900

    Update main-18.c

--- Ch6/main-18.c
+++ Ch6/main-18.c
@@ -4,6 +4,7 @@ int main()
 {
     int num = 1;
     int sum = 0; //초기값이 없으면? Garbage값이 들어감!
+    //sum을 초기값을 0을 만든 이유: num들을 while문으로 반복해서 더하기 때문에 빈 값이 필요한것
     
     while(num <= 10){
         sum += num;
