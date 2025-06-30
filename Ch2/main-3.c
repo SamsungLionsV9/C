5408b39c7bd70778a0beb5bf0a5789a897dbea3f
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-06-30 12:41:57 +0900

    Update main-3.c

--- Ch2/main-3.c
+++ Ch2/main-3.c
@@ -2,11 +2,12 @@
 
 int main()
 {
-    int total = 89;
-    float average = 95.2f;
+    int total = 89; //정수 변수 total선언
+    float average = 95.2f; //실수 변수 average선언 (double형이면 95.2 그대로 개시)
+    //C언어에서는 double이 기본 실수형임.
     
     printf("총점: %d\n", total);
-    printf("평균: %.2f\n", average);
+    printf("평균: %.2f\n", average); //소수점 2자리까지 실수를 출력한다는 의미
     printf("총점: %d\n, 평균: .2f\n" total, average);
     
     return 0;
