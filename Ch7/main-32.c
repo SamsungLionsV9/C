38e672adff333f5e4df0d1d5ff2c03dac11e522e
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 02:40:45 +0900

    Update main-32.c

--- Ch7/main-32.c
+++ Ch7/main-32.c
@@ -3,12 +3,12 @@
 
 int isPrime(int num);
 int isPrime(int num){
-    if(num<=1) return 0;
+    if(num<=1) return 0; //소수 판별이 불가능한 수라는 뜻
     
     for (int i = 2; i <= sqrt(num); i++){
-        if(num % i == 0) return 0;
-    }
-    return 1;
+        if(num % i == 0) return 0; //소수 판별문제
+    }                        //i는 2부터 시작하며 판별할 제곱수(숫자)는 i보다 커야함, 만약 i로 나눠떨어지면 소수가 아니므로 0을반환
+    return 1; //소수임이 확정되므로 출력하고 반환한다.
 }
 
 
@@ -20,7 +20,7 @@ int main(){
     scanf("%d", &num);
     
     if(isPrime(num)){
-        printf("%d는 소수입니다.\n", num);
+        printf("%d는 소수입니다.\n", num); //return1일때(소수일때) 출력된다.
     }
     else{
         printf("%는 소수가 아닙니다.\n", num);
