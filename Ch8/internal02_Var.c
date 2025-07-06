e402ac9473619331fa540fb7d6a51f8e6306893c
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 02:51:17 +0900

    Update internal02_Var.c

--- Ch8/internal02_Var.c
+++ Ch8/internal02_Var.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-//extern in internalVar;
+//extern in internalVar; //이 부분이 주석되어있는 이유: 메인파일에서는 정적 전역변수라서 링커 에러가 발생함
 
 extern void printInternalVar();
 extern void modifyInternalVar(int value);
 
 int main(){
-    printInternalVar();
-    modifyInternalVar(50);
-    printInternalVar();
+    printInternalVar(); //메인파일에서 쓰는 함수를 사용하겠다는 의미
+    modifyInternalVar(50); // 외부 함수에 값을 전달하여 정적 전역 변수의 값을 수정함
+    printInternalVar(); //그 값을 출력함
     return 0;
 }
