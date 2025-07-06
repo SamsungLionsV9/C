8c958c0ff70ab48ea5e1c40741a4b12d412c75a1
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 02:09:20 +0900

    Update main-24.c

--- Ch6/main-24.c
+++ Ch6/main-24.c
@@ -6,7 +6,7 @@ int main()
     
     for(i = 0; i <= 5; i++){
         for(j = 0; j < i ; j++){
-            printf("*");        
+            printf("*");        //i가 커질수록 *의 출력 개수가 늘어난다. 왼쪽부터 출력되는 형태
         }
         printf("\n");
     }
