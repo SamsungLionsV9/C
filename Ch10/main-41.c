de29cb4da3f649f7c8d85531f6f29e6d59ba7208
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 03:35:58 +0900

    Update main-41.c

--- Ch10/main-41.c
+++ Ch10/main-41.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-void printValue(void *ptr, char type){
+void printValue(void *ptr, char type){ //void *ptr로 어떤 타입이든 받을 수 있다는 것, char type은 어떤 타입인지 힌트
     switch(type) {
-        case 'i':
+        case 'i':        //i, f, c에 char type이 사용됨
             printf("%d\n", *(int*)ptr);
             break;
         case 'f':
@@ -13,7 +13,7 @@ void printValue(void *ptr, char type){
             break;
     }
 }
-
+void *ptr을 쓰는 이유는 다양한 타입의 값을 하나의 함수로 처리하고 싶을때 사용한다.
 
 int main(){
     int i = 23;
@@ -23,6 +23,6 @@ int main(){
     printValue(&i, 'i');
     printValue(&f, 'f');
     printValue(&c, 'c');
-
+                        //그리고 그 값들을 출력함
     return 0;
 }
