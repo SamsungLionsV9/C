ee03c799b453a3438d9e1f06c41fd913f5007929
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 02:48:10 +0900

    Update internal01_Var.c

--- Ch8/internal01_Var.c
+++ Ch8/internal01_Var.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-static int internalVar = 20;
+static int internalVar = 20; //정적 전역변수를 선언
 
-void printInternalVar(){
-    printf("internalVar: %d\n", internalVar);
+void printInternalVar(){ 
+    printf("internalVar: %d\n", internalVar); //함수를 정의 20을 출력함, 여기서는 void함수이다.
 }
 
 void modifyInternalVar(int value){
-    internalVar = value;
+    internalVar = value; //value라는 새로운 값을 입력받음 그걸 이제 상단 전역변수의 값으로 수정
 }
 
 /*int main(){
