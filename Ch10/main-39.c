7557ae8f48f65f93c9890aa0aaa447dfe40ce2ca
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 03:29:56 +0900

    Update main-39.c

--- Ch10/main-39.c
+++ Ch10/main-39.c
@@ -5,10 +5,10 @@ int main(){
     int *ptr;
     ptr = &value;
     
-    printf("value의 값: %d\n", value);
-    printf("value의 주소: %p\n", &value);
-    printf("ptr이 가리키는 주소: %p\n", ptr);
-    printf("ptr이 가리키는 실제값: %d\n", *ptr);
+    printf("value의 값: %d\n", value); //125
+    printf("value의 주소: %p\n", &value); //16진수의 메모리 주소값
+    printf("ptr이 가리키는 주소: %p\n", ptr); //16진수의 메모리 주소값
+    printf("ptr이 가리키는 실제값: %d\n", *ptr); //125
 
     return 0;
 }
