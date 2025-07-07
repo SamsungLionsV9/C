c46b608fbd839fe040b952a63725c8d5e8676965
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 14:41:33 +0900

    Update main-40.c

--- Ch10/main-40.c
+++ Ch10/main-40.c
@@ -6,7 +6,7 @@ int main(){
     
     pnum = &num; //pnum에 num을 대입
     
-    printf("*(pnum) = %d \n", *(pnum)); //pnum, 즉 num의 주소를 출력
+    printf("*(pnum) = %d \n", *(pnum)); //pnum, 즉 num의 주소에 있는 값(50)을 출력
     printf("*(pnum++) = %d \n", *(pnum++)); //50이 출력, pnum에 1을 더한 값이 후위연산될예정
     printf("*(++pnum) = %d \n", *(++pnum)); //여기서 후위연산된 값에다가 전위연산을 하여 쓰레기값이 출력됨
     
