5f3fa026a1600750a89eb64d41182e0f70a1aa4f
SamsungLionsV9 <jey010102@gachon.ac.kr>
2025-07-07 11:17:23 +0900

    Update main-33.c

--- Ch8/main-33.c
+++ Ch8/main-33.c
@@ -3,12 +3,12 @@
 #include <time.h>
 
 int generateNumber(int n1, int n2){
-    int randNum; //매개변수는 rand로 만들어진 수를 의미
+    int randNum; //매개변수 n1, n2는 rand로 만들어진 수를 의미
         
     while(1){
         randNum = rand() % 9 + 1; //1부터 9까지를 출력하겠다는 의미
         if(randNum != n1 && randNum != n2){ //1~9중에서 중복되지 않게 숫자를 생성해줌
-            break;     
+            break;     //난수가 중복되지 않으면 break; -> 출력
         }
     }        return randNum; //그 수를 출력함
 }
@@ -20,17 +20,17 @@ int getInput(int prompt){
     return input; //반환
 }
 int checkGuess(int com1, int com2, int com3, int user1, int user2, int user3){
-    int strike = 0, ball = 0;
+    int strike = 0, ball = 0; //com1~com3: generateNumber에서 생성된 난수, user1~3: getInput에서 입력받은 함수
     
-    if(user1 == com1){ strike++;}  //맞춘경우
+    if(user1 == com1){ strike++;}  //맞춘경우(난수 == 입력값)
     if(user2 == com2){ strike++;}
     if(user3 == com3){ strike++;}
     
-    if(user1 == com2 || user1 == com3) ball++; //틀린경우
-    if(user2 == com1 || user2 == com3) ball++;
-    if(user3 == com1 || user3 == com2) ball++;
+    if(user1 == com2 || user1 == com3) {ball++;} //틀린경우(난수 != 입력값)
+    if(user2 == com1 || user2 == com3) {ball++;}
+    if(user3 == com1 || user3 == com2) {ball++;}
     
-    printf("결과: %d 스트라이크, %d 볼\n", strike, ball);
+    printf("결과: %d 스트라이크, %d 볼\n", strike, ball); //위에서 누적된 strike와 ball의 값을 받아와서 출력
     return strike;
 }
 
@@ -48,7 +48,7 @@ void playGame(){
     
     while(1){
         user1 = getInput(1); //입력한 수 1~3
-        user2 = getInput(2);
+        user2 = getInput(2);    //여기서 getInput()내에 있는 값(1~3)이 매개변수 int prompt의 값이 된다.
         user3 = getInput(3);
         
         if(user1 < 1 || user1 > 9 || user2 < 1 || user2 > 9 || user3 < 1 || user3 > 9){
@@ -67,8 +67,8 @@ void playGame(){
     }
 }
 int main(){
-    srand(time(NULL));
-    playGame();
+    srand(time(NULL)); //generateNumber 함수 안에 있는 rand함수에 영향을 준다
+    playGame(); //playGame함수를 호출한다는 의미
     return 0;
 }
 
